4fba7aed3a37fb1ad458d414a87522e719dd9047
DryoX <40540721+DryoX@users.noreply.github.com>
2018-12-13 00:36:35 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-exports.TOKRN = `NTIyMzc4NDM3NjIxNTc5Nzc2.DvLH3Q.JJdwaikrpcYjY6491Ue3U6YJeRo`;
+exports.TOKEN = `NTIyMzc4NDM3NjIxNTc5Nzc2.DvLH3Q.JJdwaikrpcYjY6491Ue3U6YJeRo`;
 
 exports.PREFIX = `p!`;
